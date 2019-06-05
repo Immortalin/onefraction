4afd51553e70fccf21144ee65fc5d4d181ef640a
Elie Steinbock <eliesteinbock@gmail.com>
2019-06-04 20:31:14 -0700

    spelling fix

--- server/src/modules/user/UserService.ts
+++ server/src/modules/user/UserService.ts
@@ -1,27 +1,27 @@
 import { ModelType } from 'typegoose'
-import UserModal, { User } from './UserEntity'
+import UserModel, { User } from './UserEntity'
 
 export class UserService {
-  private readonly modal: ModelType<User>
+  private readonly model: ModelType<User>
 
   constructor() {
-    this.modal = UserModal
+    this.model = UserModel
   }
 
   async find(selector?: Partial<User>) {
-    return this.modal.find(selector)
+    return this.model.find(selector)
   }
 
   async findOneById(_id: string) {
-    return this.modal.findOne({ _id })
+    return this.model.findOne({ _id })
   }
 
   async remove(_id: string) {
-    let entityToRemove = await this.modal.findOne(_id)
-    await this.modal.remove(entityToRemove)
+    let entityToRemove = await this.model.findOne(_id)
+    await this.model.remove(entityToRemove)
   }
 
   async count(entity: any) {
-    return this.modal.count(entity)
+    return this.model.count(entity)
   }
 }
